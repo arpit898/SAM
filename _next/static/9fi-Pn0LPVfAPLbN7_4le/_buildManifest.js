b0ba69130182a3f92acc1f8375154eeff34aa45c
arpit898 <arpit898@users.noreply.github.com>
2026-05-29 02:55:19 +0000

    deploy: 5f75223e1487cc6424a636fbb74a9eb9a269e7ce

---
+++ _next/static/9fi-Pn0LPVfAPLbN7_4le/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/SAM//_next/:path+",
+        "destination": "/SAM/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
